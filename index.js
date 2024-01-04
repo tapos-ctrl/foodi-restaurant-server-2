d8fbcc201f1e6ff78caaa325d791a3f56bdd25c1
Tapos Chandro <taposchandro231@gmail.com>
2024-01-04 21:34:51 +0600

    cors maidleware set

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 5000
 
 
 app.use(cors({
-  origin:['https://frodi-restaurant.web.app/allFood']
+  origin:['https://frodi-restaurant.web.app/allFood','https://frodi-restaurant.web.app/order']
 }))
 
 
